b04f1d583d487e7d0a251c326c244a7656e7bdd2
rares vasiliu <rv.vasiliu@gmail.com>
2025-10-03 19:16:42 +0200

    feat: test case
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/main/kotlin/TestClass.kt
@@ -0,0 +1,8 @@
+class  TestClass   {   // extra spaces
+
+    fun sayHello( ) {   // wrong spacing
+        println( "Hello, Ktlint!" ) // space after (
+    }
+
+    fun add(a:Int , b :Int ):Int { return a+b }  // spacing + compact function
+}
